fbac9e2d74ba68c9d61d9af22c649f3f1f4de77a
Arthur Felipe <arthurfelipegaspar09@gmail.com>
2026-06-11 16:00:31 -0300

    Adicionada interface web com Flask

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
+from flask import Flask, render_template, request
 import pyshorteners
 
+app = Flask(__name__)
 
-url = input("Digite uma URL para encurtar: ")
+@app.route("/", methods=["GET", "POST"])
+def index():
+
+    url_curta = None
+
+    if request.method == "POST":
+
+        url = request.form["url"]
+
+        try:
+            encurtador = pyshorteners.Shortener()
+            url_curta = encurtador.tinyurl.short(url)
+
+        except Exception:
+            url_curta = "Erro ao encurtar a URL."
+
+    return render_template("index.html", url_curta=url_curta)
 
 
-encurtador = pyshorteners.Shortener()
-
-url_curta = encurtador.tinyurl.short(url)
-
-
-print(f"Link original: {url}")
-
-print(f"Link encurtado: {url_curta}")
+if __name__ == "__main__":
+    app.run(debug=True)
